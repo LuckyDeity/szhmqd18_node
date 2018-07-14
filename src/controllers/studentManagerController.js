921e5fe36fbd82d94e1bc06bd19d44e3d2ad0794
kakaxi <995256454@qq.com>
2018-07-14 10:40:01 +0800

    05-完成后台管理页面-list

--- src/controllers/studentManagerController.js
+++ src/controllers/studentManagerController.js
@@ -1,6 +1,33 @@
-const xtpl = require('xtpl')
-const path = require('path')
+const xtpl = require("xtpl");
+const path = require("path");
+const MongoClient = require("mongodb").MongoClient;
 
-exports.getStudentListPage = (req,res) => {
-    res.sendFile(path.join(__dirname, "../views/list.html"));
-}
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = "szhmqd18";
+
+exports.getStudentListPage = (req, res) => {
+    const keyword = req.query.keyword || ''
+  MongoClient.connect(
+    url,
+    {useNewUrlParser:true},
+    function(err, client) {
+      const db = client.db(dbName);
+
+      const collection = db.collection("studentInfo");
+      // Find some documents
+      collection.find({ name: {$regex:keyword} }).toArray(function(err, docs) {
+          client.close();
+
+          xtpl.renderFile(
+            path.join(__dirname, "../views/list.html"),{studentList:docs,keyword},(err, content) => {
+              res.send(content);
+            }
+          );
+
+      });
+    }
+  );
+};
--- src/routers/studentManagerRouter.js
+++ src/routers/studentManagerRouter.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require('path')
 
-const studentManagerRouter = express.Router();
+const studentManagerRouter = express.Router()
 
 const studentManagerController = require(path.join(__dirname,"../controllers/studentManagerController.js"))
 
